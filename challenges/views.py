55182708a5f32d9398470eedc616b0e4921ebd0e
luke francis <lukebrandonfrancis@gmail.com>
2023-02-07 13:05:56 -0500

    deleted comment

--- challenges/views.py
+++ challenges/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse 
 from django.template.loader import render_to_string
 
-# Create your views here.
 
 monthly_challenges = {
     "january": "eat no meat for the entire month !",
@@ -17,23 +16,27 @@ monthly_challenges = {
     "september": "do some gardening every weekend",
     "october": "go fishing everyday",
     "november": "read a book everyday",
-    "december": "go to the gym at least 5 times a week"
+    "december": None
 }
 
 def index(request):
-    list_items = ""
+    
     months = list(monthly_challenges.keys())
 
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+    
     for month in months:
         capitalized_month = month.capitalize()
         month_path = reverse("month-challenge", args=[month])
         list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-    
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
     # up top function shows what it will look like on client side 
     # "<li><a href="...">January</a></li><li><a href="..."February</a></li>
     
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
